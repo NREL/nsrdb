ab339d524ecd8234f5a986914b45e95adf6a46d9
ngilroy <nicholas.gilroy@nrel.gov>
2019-03-21 14:30:07 -0600

    agg albedo

--- nsrdb/albedo/modis_albedo.py
+++ nsrdb/albedo/modis_albedo.py
@@ -12,14 +12,15 @@ import h5py
 from pyhdf.SD import SD, SDC
 import numpy as np
 import pandas as pd
-import sys
+# import sys
 import time
 # import warnings
 # from collections import OrderedDict
 import psutil
 from scipy.spatial import cKDTree
 from configobj import ConfigObj
-from scoop import futures
+# from scoop import futures
+# from dask.distributed import Client
 # import linecache
 import copy
 # from scoop import shared
@@ -126,27 +127,34 @@ class RetrieveMODIS:
 
 class Aggregate(object):
     """Class to aggregate MODIS variables to NSRDB grid"""
-    def __init__(self, config, years, days):
+    def __init__(self, config, years):
         """
         Parameters
         ----------
         config : ini
             config file for aggregating MODIS albedo to NSRDB grid.
-        years : range(2003, 2015)
-            Years to process.
+        years : int
+            Year to process.
         days : str
             Eight day periods that alebdo data is processed at ('001').
         """
-        self.config = config
+        self.config = ConfigObj(config, unrepr=True)
+        logger.debug(self.config)
+
         self.years = years
-        self.days = days
+        self.days = [str(d).zfill(3) for d in range(1, 362, 8)]
 
     def sparseToSpatial(self):
-        """Sparse to Spatial."""
-        with h5py.File(
-            os.path.join(self.config['dir']['nsrdb_dir'],
-                         self.config['fileNames']['nsrdb']
-                         ), 'r') as hfile:
+        """Sparse to Spatial: Taking meta from HDF5 nsrdb 1998
+
+        !!!Eventually: Take the meta from the extent files!!!
+
+        """
+        fname = os.path.join(
+            self.config['dir']['nsrdb_dir'], self.config['fileNames']['nsrdb'])
+        logger.debug('reading{}'.format(fname))
+
+        with h5py.File(fname, 'r') as hfile:
             self.meta = hfile['meta'][...]
         coords = pd.DataFrame(
             {'lon': np.round(self.meta['longitude'] * 100, 0).astype(np.int16),
@@ -192,7 +200,10 @@ class Aggregate(object):
         self.grid.loc[np.isnan(self.grid.loc[:, 'h5_order']), 'lat_ind'] = -99
 
     def getHDF4meta(self):
-        """Get a meta for the HDF4."""
+        """
+        Get a meta for the HDF4.
+        Function grabs the albedo metadata.
+        """
         try:
             clipping_frame = (
                 self.h5gtform[0] - self.h5gtform[2] / 2,
@@ -203,7 +214,7 @@ class Aggregate(object):
                 self.config['dir']['in_dir'],
                 self.config['fileNames']['inName'].format(
                     var=self.config['variables']['variable_names'][0],
-                    day='001', year=self.years[0]))
+                    day='001', year=self.years))
             hf = SD(fname, SDC.READ)
             lon4 = hf.select('Longitude')[:]
             lon4[lon4 > 0] = lon4[lon4 > 0] - 360
@@ -218,11 +229,14 @@ class Aggregate(object):
         except Exception as e:
             # logger = logging.getLogger('model_run.getHDF4data')
             # logger.info(PrintException())
-            logger.info(e)
+            logger.exception(e)
             logger.info(fname)
 
     def getAggregationIndices(self):
-        """Retrieve indicies for aggregation."""
+        """
+        Retrieve indicies for aggregation. (Really Assiging nearest pixel).
+        This function does a cKDTree mapping the albedo data to the nsrdb data.
+        """
         tree = cKDTree(
             np.unique(self.grid['lon']).reshape(
                 np.unique(self.grid['lon']).size, 1))
@@ -267,7 +281,7 @@ class Aggregate(object):
             # logger = logging.getLogger('model_run.getHDF4data')
             # logger.info(PrintException())
             logger.info(e)
-            logger.info(fname)
+            logger.exception(fname)
 
     def write_results(self, year, var, results):
         """Write results to HDF."""
@@ -283,15 +297,14 @@ class Aggregate(object):
     def processYear(self, fname):
         """Process a year."""
         try:
-            days = [str(d).zfill(3) for d in range(1, 362, 8)]
             year = fname[-8:-4]
             splt = fname.split('_')
             var = '{var}_{band}'.format(var=splt[1], band=splt[2])
             fnames = (
                 [os.path.join(self.config['dir']['in_dir'],
-                 fname.format(day=day)) for day in days])
+                 fname.format(day=day)) for day in self.days])
             results = {'means': np.zeros(
-                shape=(len(days), self.meta.shape[0]), dtype=np.float32)}
+                shape=(len(self.days), self.meta.shape[0]), dtype=np.float32)}
             t0 = time.time()
             mem = np.max(memory_usage(-1, interval=.2, timeout=1))
             for i, fpath in enumerate(fnames):
@@ -315,10 +328,9 @@ class Aggregate(object):
             # logger = logging.getLogger('model_run.processYear')
             logger.info('exception')
             # logger.info(PrintException())
-            logger.info(e)
+            logger.exception(e)
 
-    @classmethod
-    def main(cls, log_level='INFO'):
+    def main(self, log_level='DEBUG'):
         """Run processing with test filepaths.
 
         Parameters
@@ -330,54 +342,71 @@ class Aggregate(object):
         log_level : str
             Logging level for this module (DEBUG or INFO).
         """
-        init_logger(__name__, log_file='ims.log', log_level=log_level)
+        init_logger(__name__, log_file='modis.log', log_level=log_level)
         init_logger('nsrdb.utilities.file_utils', log_file=None,
                     log_level=log_level)
         try:
             t0 = time.time()
-            config = ConfigObj(
-                '{config_file}.ini'.format(
-                    config_file=sys.argv[1]), unrepr=True)
-            years = [str(y) for y in range(
-                config['variables']['years'][0],
-                config['variables']['years'][1] + 1)]
-            days = [str(d).zfill(3) for d in range(1, 362, 8)]
-
-            out_vars = [config['fileNames']['outName'].format(
+            out_vars = [self.config['fileNames']['outName'].format(
                 var=var,
-                year=year)
-                for year in years
-                for var in config['variables']['variable_names']]
-            files = os.listdir(config['dir']['out_dir'])
+                year=self.years)
+                for var in self.config['variables']['variable_names']]
+            files = os.listdir(self.config['dir']['out_dir'])
             out_vars = [var for var in out_vars if var not in files]
             out_vars = [(fname[-7:-3], '{var}_{band}'.format(
                 var=fname.split('_')[1],
                 band=fname.split('_')[2])) for fname in out_vars]
+            logger.debug(out_vars)
 
-            vars = [config['fileNames']['inName'].format(
+            vars = [self.config['fileNames']['inName'].format(
                 var=pair[1],
                 day='{day}',
                 year=pair[0]) for pair in out_vars]
 
             logger.info(vars)
-            ag = Aggregate(config, years, days)
-            ag.sparseToSpatial()
+
             logger.info('sparse to spatial')
-            ag.getHDF4meta()
-            ag.getAggregationIndices()
+            self.sparseToSpatial()
+
+            logger.info('get HDF4 meta')
+            self.getHDF4meta()
+
             logger.info('aggregation indices')
+            self.getAggregationIndices()
+
             logger.debug(
                 'Memory: %s', str(np.max(memory_usage(
                     -1, interval=.2, timeout=1))))
-            # list(futures.map(ag.processYear, vars))
-            for result in futures.map_as_completed(ag.processYear, vars):
-                logger.info('%s complete, time: %s, memory: %s',
-                            result['year'], result['time'], result['memory'])
-                logger.debug(
-                    'Memory: %s', str(
-                        np.max(memory_usage(-1, interval=.2, timeout=1))))
+
+            for var in vars:
+                logger.debug('processing{}'.format(var))
+                self.processYear(var)
+
             logger.info('process complete, time: %s',
                         (time.time() - t0) / 60.0)
+
+            logger.info('Futures gathered and client is closed.')
+            logger.info(mem_str())
         except Exception as e:
             # logger.info(PrintException())
-            logger.info(e)
+            logger.exception(e)
+
+    @classmethod
+    def run(cls, config, years):
+        """Retrieve MODIS albedo source data for a single year.
+        Parameters
+        ----------
+        year : int | str
+            Year to download modis data (last year is currently 2015).
+        target_path : str
+            Directory to save the downloaded files.
+        Returns
+        -------
+        failed : list
+            List of files that failed to download.
+        """
+        init_logger(__name__, log_file=None, log_level='DEBUG')
+
+        modis = cls(config, years)
+        agg = modis.main()
+        return agg
