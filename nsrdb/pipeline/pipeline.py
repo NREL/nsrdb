be3eb49a1c232b11fd951ccb5699bc58d328c840
grantbuster <grant.buster@nrel.gov>
2020-12-08 11:51:39 -0700

    custom nsrdb pipeline config that wont run reV preflight for module configs

--- nsrdb/pipeline/pipeline.py
+++ nsrdb/pipeline/pipeline.py
@@ -4,6 +4,8 @@ NSRDB data pipeline architecture.
 """
 import logging
 from reV.pipeline.pipeline import Pipeline
+from rex.utilities.loggers import init_logger
+from nsrdb.pipeline.config import NsrdbPipelineConfig
 
 
 logger = logging.getLogger(__name__)
@@ -22,3 +24,24 @@ class NsrdbPipeline(Pipeline):
                 'collect-flist',
                 'collect-final',
                 )
+
+    def __init__(self, pipeline, monitor=True, verbose=False):
+        """
+        Parameters
+        ----------
+        pipeline : str | dict
+            Pipeline config file path or dictionary.
+        monitor : bool
+            Flag to perform continuous monitoring of the pipeline.
+        verbose : bool
+            Flag to submit pipeline steps with -v flag for debug logging
+        """
+        self.monitor = monitor
+        self.verbose = verbose
+        self._config = NsrdbPipelineConfig(pipeline)
+        self._run_list = self._config.pipeline_steps
+        self._init_status()
+
+        # init logger for pipeline module if requested in input config
+        if 'logging' in self._config:
+            init_logger('reV.pipeline', **self._config['logging'])
