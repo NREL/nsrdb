6b6d577443f23ec30e4f4b107ab41ab373592912
grantbuster <grant.buster@nrel.gov>
2021-06-21 10:17:37 -0600

    added initial gfs handler test

---
+++ tests/test_data_model_gfs.py
@@ -0,0 +1,46 @@
+# pylint: skip-file
+"""
+PyTest file for PV generation in Rhode Island.
+
+Created on Thu Nov 29 09:54:51 2018
+
+@author: gbuster
+"""
+
+import os
+import pytest
+import numpy as np
+import pandas as pd
+import h5py
+import datetime
+
+from nsrdb import TESTDATADIR, DEFAULT_VAR_META, DATADIR
+from nsrdb.data_model import DataModel, VarFactory
+from rex.utilities.loggers import init_logger
+
+
+RTOL = 0.01
+ATOL = 0.0
+
+DATE_16 = datetime.date(year=2021, month=6, day=16)
+DATE_9 = datetime.date(year=2021, month=6, day=9)
+
+
+def test_ancillary_single(var, date):
+    """Test GFS processed variables"""
+
+    grid = os.path.join(TESTDATADIR, 'reference_grids/', 'west_psm_extent.csv')
+
+    # set test directory
+    source_dir = os.path.join(TESTDATADIR, 'gfs_source_files')
+
+    factory_kwargs = {'air_temperature': {'source_dir': source_dir,
+                                          'handler': 'GfsVar'}}
+
+    data = DataModel.run_single(var, date, grid, factory_kwargs=factory_kwargs)
+
+
+if __name__ == '__main__':
+    init_logger('nsrdb.data_model', log_file=None, log_level='DEBUG')
+    test_ancillary_single('air_temperature', DATE_9)
+    test_ancillary_single('air_temperature', DATE_16)
