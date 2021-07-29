a39ddfb7def949fe35a3f23741f9bc765574982b
Michael Rossol <mrossol@gmail.com>
2021-07-28 20:36:56 -0600

    copy file if written to a tempdir

--- lambda.py
+++ lambda.py
@@ -1,7 +1,7 @@
 """
 Lambda function handler
 """
-from cloud_fs.filesystems import S3
+from cloud_fs import FileSystem
 from datetime import date
 from nsrdb import NSRDB
 import os
@@ -19,19 +19,32 @@ def handler(event, context):
     context : dict
         The context in which the function is called.
     """
-    day = date.today().strftime("%Y%m%d")
+    print(context)
+    day = event.get('date', None)
+    if not day:
+        day = date.today().strftime("%Y%m%d")
+
     grid = 's3://puerto-rico-nrel/puerto-rico/puerto_rico_2km_meta.csv'
     grid = event['grid']
     var_meta = 's3://puerto-rico-nrel/puerto-rico/puerto_rico_vars.csv'
     var_meta = event['var_meta']
-    freq = '5min'
-    freq = event['freq']
+    freq = event.get('freq', '5min')
 
     data_model = NSRDB.run_full(day, grid, freq, var_meta=var_meta,
                                 log_level='DEBUG')
-    out_dir = event['dout']
-    fpath_out = os.path.join(out_dir, f'puerto-rico-{day}.h5')
+
+    temp_dir = event.get('temp_dir', None)
+    out_dir = event['out_dir']
+    if temp_dir is None:
+        temp_dir = out_dir
+
+    fpath = f'puerto-rico-{day}.h5'
+    fpath_out = os.path.join(temp_dir, fpath)
     dump_vars = ['ghi', 'dni', 'dhi',
                  'clearsky_ghi', 'clearsky_dni', 'clearsky_dhi']
     for v in dump_vars:
         data_model.dump(v, fpath_out, None, mode='a')
+
+    if temp_dir != out_dir:
+        dst_path = os.path.join(out_dir, fpath)
+        FileSystem.copy(fpath_out, dst_path)
