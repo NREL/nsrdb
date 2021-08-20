49ffd21a2d4d8fcec21c2f693eb9a9ddfea14040
Michael Rossol <mrossol@gmail.com>
2021-08-20 15:31:25 -0600

    make handler importable

---
+++ lambda_test.py
@@ -0,0 +1,14 @@
+"""
+Lambda function handler tester
+"""
+from _lambda import handler
+from rex import safe_json_load
+import sys
+import time
+
+if __name__ == '__main__':
+    event = safe_json_load(sys.argv[1])
+    ts = time.time()
+    handler(event, None)
+    print('NSRDB lambda runtime: {:.4f} minutes'
+          .format(time.time() - ts / 60))
