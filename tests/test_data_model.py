c5c3ec99e32c26651cc637e80457032f0248c4f0
grantbuster <grant.buster@nrel.gov>
2021-09-29 10:55:40 -0600

    added relative diff outputs

--- tests/test_data_model.py
+++ tests/test_data_model.py
@@ -95,14 +95,19 @@ def test_ancillary_single(var):
                 var, var_meta=var_meta, date=date)
             data_baseline = var_obj.scale_data(data_baseline)
 
+        bad = ~np.isclose(data_baseline, data, atol=ATOL, rtol=RTOL)
         diff = np.abs(data_baseline - data)
+        rel_diff = np.abs(data_baseline - data) / data_baseline
         mean_baseline = np.mean(data_baseline)
         mean_test = np.mean(data)
-        msg = ('Data for "{}" has abs diff of: max {}, mean {}, min {} '
+        msg = ('Data for "{}" has abs diff of: max {}, mean {}, min {}, '
+               'relative abs diff of: max {}, mean {}, min {} '
                'with a mean baseline data value of: {} and mean test data '
-               'value of: {}'
+               'value of: {}. \nBad baseline values: {}, \nbad test values: {}'
                .format(var, diff.max(), diff.mean(), diff.min(),
-                       mean_baseline, mean_test))
+                       rel_diff.max(), rel_diff.mean(), rel_diff.min(),
+                       mean_baseline, mean_test,
+                       data_baseline[bad], data[bad]))
 
         assert np.allclose(data_baseline, data, atol=ATOL, rtol=RTOL), msg
 
