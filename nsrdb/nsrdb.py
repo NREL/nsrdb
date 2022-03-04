9ca59cebc529c00053fdec5995d8cc673957c5d0
bnb32 <brandon.benton@nrel.gov>
2022-03-04 11:25:12 -0800

    more cleaning

--- nsrdb/nsrdb.py
+++ nsrdb/nsrdb.py
@@ -132,24 +132,20 @@ class NSRDB:
         user_input = copy.deepcopy(default_kwargs)
         user_input.update(kwargs)
 
-        if user_input['extent'] == 'full':
-            user_input['extent_tag'] = 'RadF'
-        if user_input['extent'] == 'conus':
-            user_input['extent_tag'] = 'RadC'
-
         if user_input['year'] < 2018:
             user_input['extent'] = 'full'
             user_input['spatial'] = '4km'
-            meta_lon = -105
+
+        extent_tag_map = {'full': 'RadF', 'conus': 'RadC'}
+        user_input['extent_tag'] = extent_tag_map[user_input['extent']]
+
+        meta_lon_map = {'full': -105, 'conus': -113}
+        meta_lon = meta_lon_map[user_input['extent']]
 
         if user_input['meta_file'] is None:
             meta_file = f'nsrdb_meta_{user_input["spatial"]}'
 
             if user_input['year'] > 2017:
-                if user_input['extent'] == 'conus':
-                    meta_lon = -113
-                elif user_input['extent'] == 'full':
-                    meta_lon = -105
                 meta_file += f'_{user_input["extent"]}'
 
             meta_file += f'_{user_input["satellite"]}_{meta_lon}.csv'
