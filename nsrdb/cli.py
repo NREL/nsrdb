b5d823e278ca8dd786ef3bd700dee51892fdd307
bnb32 <brandon.benton@nrel.gov>
2022-03-28 08:59:07 -0700

    string issues

--- nsrdb/cli.py
+++ nsrdb/cli.py
@@ -171,8 +171,8 @@ def blend(ctx, kwargs, collect, eagle):
             user_input.update(kwargs)
             stdout_path = user_input.get('stdout', './')
 
-            cmd = ("python -c from nsrdb.nsrdb import NSRDB;"
-                   f"NSRDB.collect_blended({kwargs})")
+            cmd = ("python -c \"from nsrdb.nsrdb import NSRDB;"
+                   f"NSRDB.collect_blended({kwargs})\"")
 
             slurm_manager = SLURM()
 
@@ -251,8 +251,8 @@ def aggregate(ctx, kwargs, collect, eagle):
             user_input.update(kwargs)
             stdout_path = user_input.get('stdout', './')
 
-            cmd = ("python -c \'from nsrdb.nsrdb import NSRDB;"
-                   f"NSRDB.collect_aggregation(\'{kwargs}\')\'")
+            cmd = ("python -c \"from nsrdb.nsrdb import NSRDB;"
+                   f"NSRDB.collect_aggregation({kwargs})\"")
 
             slurm_manager = SLURM()
 
