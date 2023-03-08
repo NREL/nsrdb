65fb64c5865b2d25ac9f1187331c167575f45a2e
xieyupku <xieyupku@gmail.com>
2023-03-08 13:07:05 -0700

    revise the all sky model for using FARMS-DNI

--- nsrdb/all_sky/all_sky.py
+++ nsrdb/all_sky/all_sky.py
@@ -154,7 +154,7 @@ def all_sky(alpha, aod, asymmetry, cloud_type, cld_opd_dcomp, cld_reff_dcomp,
     rest_data.ghi = dark_night(rest_data.ghi, solar_zenith_angle, lim=SZA_LIM)
 
     # use FARMS to calculate cloudy GHI
-    ghi = farms(tau=cld_opd_dcomp,
+    ghi, dni_farmsdni, dni0 = farms(tau=cld_opd_dcomp,
                 cloud_type=cloud_type,
                 cloud_effective_radius=cld_reff_dcomp,
                 solar_zenith_angle=solar_zenith_angle,
@@ -179,8 +179,13 @@ def all_sky(alpha, aod, asymmetry, cloud_type, cld_opd_dcomp, cld_reff_dcomp,
                pressure=surface_pressure)
 
     # merge the clearsky and cloudy irradiance into all-sky irradiance
-    dni = merge_rest_farms(rest_data.dni, dni, cloud_type)
+################
+#    dni = merge_rest_farms(rest_data.dni, dni, cloud_type)
 
+    dni = merge_rest_farms(rest_data.dni, dni_farmsdni, cloud_type)
+#    dni = merge_rest_farms(rest_data.dni, dni0, cloud_type)
+
+################
     # make a fill flag where bad data exists in the GHI irradiance
     fill_flag = make_fill_flag(ghi, rest_data.ghi, cloud_type, missing_props,
                                cloud_fill_flag=cloud_fill_flag)
