1cd04a34965d46dce7d8ca16edf5a3ea6c6e3150
xieyupku <xieyupku@gmail.com>
2023-03-30 08:40:24 -0600

    FARMS-DNI

--- nsrdb/all_sky/all_sky.py
+++ nsrdb/all_sky/all_sky.py
@@ -189,7 +189,7 @@ def all_sky(alpha, aod, asymmetry, cloud_type, cld_opd_dcomp, cld_reff_dcomp,
 
     # merge the clearsky and cloudy irradiance into all-sky irradiance
     #
-    # Use the DNI computed by FARMS-DNI. Updated by Yu Xie on 3/29/203. 
+    # Use the DNI computed by FARMS-DNI. Updated by Yu Xie on 3/29/203.
     #    dni = merge_rest_farms(rest_data.dni, dni, cloud_type)
     #    dni = merge_rest_farms(rest_data.dni, dni0, cloud_type)
     dni = merge_rest_farms(rest_data.dni, dni_farmsdni, cloud_type)
