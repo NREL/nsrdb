250452cd6da1e4e6a7667f8ae080cf946a672957
bnb32 <brandon.benton@nrel.gov>
2023-08-22 10:56:45 -0600

    rev.config.pipeline.PipelineConfig gone so integrated into NSRDB

--- nsrdb/pipeline/config.py
+++ nsrdb/pipeline/config.py
@@ -1,18 +1,51 @@
 # -*- coding: utf-8 -*-
 """
-reV pipeline config
+NSRDB pipeline config
 
 Created on May 28 2019
 
 @author: gbuster
 """
+import os
+
 from reV.config.base_analysis_config import AnalysisConfig
-from reV.config.pipeline import PipelineConfig
-from reV.utilities.exceptions import ConfigError
+from reV.utilities.exceptions import ConfigError, PipelineError
 
 
-class NsrdbPipelineConfig(PipelineConfig):
+class NsrdbPipelineConfig(AnalysisConfig):
     """NSRDB pipeline configuration based on reV pipeline"""
+
+    def __init__(self, config):
+        """
+        Parameters
+        ----------
+        config : str | dict
+            File path to config json (str), serialized json object (str),
+            or dictionary with pre-extracted config.
+        """
+
+        super().__init__(config, run_preflight=False)
+        self._check_pipeline()
+        self._parse_dirout()
+        self._check_dirout_status()
+
+    def _check_pipeline(self):
+        """Check pipeline steps input. ConfigError if bad input."""
+
+        if 'pipeline' not in self:
+            raise ConfigError('Could not find required key "pipeline" in the '
+                              'pipeline config.')
+
+        if not isinstance(self.pipeline, list):
+            raise ConfigError('Config arg "pipeline" must be a list of '
+                              '(command, f_config) pairs, but received "{}".'
+                              .format(type(self.pipeline)))
+
+        for di in self.pipeline:
+            for f_config in di.values():
+                if not os.path.exists(f_config):
+                    raise ConfigError('Pipeline step depends on non-existent '
+                                      'file: {}'.format(f_config))
 
     # pylint: disable=W0201
     def _parse_dirout(self):
@@ -40,3 +73,67 @@ class NsrdbPipelineConfig(PipelineConfig):
             raise ConfigError('Pipeline steps must have a unique job names '
                               'directory but received {} duplicate names.'
                               .format(len(names) - len(set(names))))
+
+    def _check_dirout_status(self):
+        """Check unique status file in dirout."""
+
+        if os.path.exists(self.dirout):
+            for fname in os.listdir(self.dirout):
+                if (fname.endswith('_status.json')
+                        and fname != '{}_status.json'.format(self.name)):
+                    msg = ('Cannot run pipeline "{}" in directory '
+                           '{}. Another pipeline appears to have '
+                           'been run here with status json: {}'
+                           .format(self.name, self.dirout, fname))
+                    raise PipelineError(msg)
+
+    @property
+    def pipeline(self):
+        """Get the pipeline steps.
+
+        Returns
+        -------
+        pipeline : list
+            reV pipeline run steps. Should be a list of (command, config)
+            pairs.
+        """
+
+        return self['pipeline']
+
+    @property
+    def logging(self):
+        """Get logging kwargs for the pipeline.
+
+        Returns
+        -------
+        dict
+        """
+        return self.get('logging', {"log_file": None, "log_level": "INFO"})
+
+    @property
+    def hardware(self):
+        """Get argument specifying which hardware the pipeline is being run on.
+
+        Defaults to "eagle" (most common use of the reV pipeline)
+
+        Returns
+        -------
+        hardware : str
+            Name of hardware that this pipeline is being run on.
+            Defaults to "eagle".
+        """
+        return self.get('hardware', 'eagle')
+
+    @property
+    def status_file(self):
+        """Get status file path.
+
+        Returns
+        -------
+        _status_file : str
+            reV status file path.
+        """
+        if self.dirout is None:
+            raise ConfigError('Pipeline has not yet been initialized.')
+
+        return os.path.join(self.dirout, '{}_status.json'.format(self.name))
